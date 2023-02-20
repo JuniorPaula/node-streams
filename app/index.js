5cc7a07ca59052315aec33ac3ee1507e70e015d2
Junior Paula <luke.junnior@icloud.com>
2023-02-20 15:48:44 -0300

    Create method to append data to HTML view

--- app/index.js
+++ app/index.js
@@ -7,14 +7,57 @@ async function consumeAPI(signal) {
     let counter =0
     const reader = response.body
         .pipeThrough( new TextDecoderStream())
-        .pipeTo(new WritableStream({
-            write(chunk) {
-                console.log(++counter, 'chunk', chunk)
-            }
-        }))
+        .pipeThrough(parseNDJSON())
+        // .pipeTo(new WritableStream({
+        //     write(chunk) {
+        //         console.log(++counter, 'chunk', chunk)
+        //     }
+        // }))
     
     return reader
 }
 
+function appendToHTML(element) {
+    return new WritableStream({
+        write({ title, decription, url_anime }) {
+            const card = `
+            <article>
+                <div class="text">
+                    <h3>${title}</h3>
+                    <p>${decription}</p>
+                    <a href="${url_anime}">Here's why</a>
+                </div>
+            </article>
+            `
+            element.innerHTML += card
+        }
+    })
+}
+
+function parseNDJSON() {
+    let ndjsonBuffer = ''
+    return new TransformStream({
+        transform(chunk, controller) {
+            ndjsonBuffer += chunk
+            const items = ndjsonBuffer.split('\n')
+            items.slice(0, -1)
+                .forEach(item => controller.enqueue(JSON.parse(item)))
+        
+            ndjsonBuffer = items[items.length -1]
+        },
+        flush(controller) {
+            if(!ndjsonBuffer) return
+            controller.enqueue(JSON.parse(ndjsonBuffer))
+        }
+    })
+}
+
+const [
+    start,
+    stop,
+    cards
+] = ['start', 'stop', 'cards'].map(item => document.getElementById(item))
+
 const abortController = new AbortController()
-await consumeAPI(abortController.signal)
+const readable = await consumeAPI(abortController.signal)
+readable.pipeTo(appendToHTML(cards))
