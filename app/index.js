6e142d104311c8575e99444a90353b27fc453083
Junior Paula <luke.junnior@icloud.com>
2023-02-20 16:10:06 -0300

    Add listeners to start and stop strems

--- app/index.js
+++ app/index.js
@@ -1,35 +1,33 @@
 const API_URL = "http://localhost:3000"
+let counter =0
 
 async function consumeAPI(signal) {
     const response = await fetch(API_URL, {
         signal
     })
-    let counter =0
     const reader = response.body
         .pipeThrough( new TextDecoderStream())
         .pipeThrough(parseNDJSON())
-        // .pipeTo(new WritableStream({
-        //     write(chunk) {
-        //         console.log(++counter, 'chunk', chunk)
-        //     }
-        // }))
     
     return reader
 }
 
 function appendToHTML(element) {
     return new WritableStream({
-        write({ title, decription, url_anime }) {
+        write({ title, description, url_anime }) {
             const card = `
             <article>
                 <div class="text">
-                    <h3>${title}</h3>
-                    <p>${decription}</p>
-                    <a href="${url_anime}">Here's why</a>
+                    <h3>[${++counter}] ${title}</h3>
+                    <p>${description}</p>
+                    <a href="${url_anime.slice(0, 100)}">Here's why</a>
                 </div>
             </article>
             `
             element.innerHTML += card
+        },
+        abort(reason) {
+            console.log('**aborted', reason)
         }
     })
 }
@@ -58,6 +56,14 @@ const [
     cards
 ] = ['start', 'stop', 'cards'].map(item => document.getElementById(item))
 
-const abortController = new AbortController()
-const readable = await consumeAPI(abortController.signal)
-readable.pipeTo(appendToHTML(cards))
+let abortController = new AbortController()
+start.addEventListener('click', async () => {
+    const readable = await consumeAPI(abortController.signal)
+    readable.pipeTo(appendToHTML(cards))
+})
+
+stop.addEventListener('click', () => {
+    abortController.abort()
+    console.log('aborting...')
+    abortController = new AbortController()
+})
